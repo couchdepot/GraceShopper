2529791cc377658eedc20cc0e985481d68766d94
Charlie Azmitia <ccazmitia@gmail.com>
2019-05-01 23:45:47 -0400

    added products model and routes

---
+++ Server/Db/Product.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize');
+const db = require('./db');
+
+const Product = db.define('product', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validation: {
+            notEmpty: true
+        }
+    },
+    description: Sequelize.TEXT,
+    price: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        validation: {
+            notEmpty: true
+        }
+    },
+    quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validation: {
+            notEmpty: true
+        }
+    },
+    imageUrl: Sequelize.STRING,
+    reviews: Sequelize.ARRAY(Sequelize.JSONB)
+});
+
+module.exports = Product;
