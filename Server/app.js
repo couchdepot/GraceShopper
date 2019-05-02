fc3f9e8b6e21b2d309e2d8b523babc4c040fbbda
Alex Bukhman <alexb823@gmail.com>
2019-05-02 21:20:10 +0000

    added the auth router and endware to the express app

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const session = require('express-session')
+const session = require('express-session');
+const { auth } = require('./Routes');
 
 // Body parsing middleware
 app.use(express.json());
@@ -13,8 +14,29 @@ app.use(session({
   saveUninitialized: false,
 }))
 
+// Authentication router
+app.use('auth', auth);
+
+
 app.get('/app.js', (req, res, next) => res.sendFile(path.join(__dirname, '..', 'dist', 'main.js')));
 
 app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
 
+
+// Handle 404s
+app.use((req, res, next) => {
+  const err = new Error('Not Found!');
+  err.status = 404;
+  next(err);
+})
+
+// Error handling endware
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send(err.message || 'Server error!')
+})
+
 module.exports = app;
+
+
+
