dfbc35f2d230e477092614ebbaa39f70e68817a0
Alex Bukhman <alexb823@gmail.com>
2019-05-02 18:33:37 +0000

    added authentication router for logging in and out

--- Server/app.js
+++ Server/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const session = require('express-session')
 
+// Body parsing middleware
 app.use(express.json());
+
+// Session middleware
+app.use(session({
+  secret: 'secret string',
+  resave: false,
+  saveUninitialized: false,
+}))
 
 app.get('/app.js', (req, res, next) => res.sendFile(path.join(__dirname, '..', 'dist', 'main.js')));
 
