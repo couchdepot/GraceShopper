5e73579516585856bf9256ca336003384226f467
Alex Bukhman <alexb823@gmail.com>
2019-05-05 16:23:09 -0400

    adde cart reducer

---
+++ src/reducers/cartReducer.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const GOT_CART = 'GOT_CART';
+
+const gotCart = cart => {
+  return {
+    type: GOT_CART,
+    cart,
+  };
+};
+
+export const cartReducer = (state = [], action) => {
+  switch (action.type) {
+    case GOT_CART:
+      return action.cart;
+    default:
+      return state;
+  }
+};
+
+// Thunks
+export const getUsersCart = (userId, status) => {
+  return dispatch => {
+    return axios
+      .get(`/api/carts/${userId}/${status}`)
+      .then(response => response.data)
+      .then(cart => dispatch(gotCart(cart)));
+  };
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,19 @@
 import { combineReducers } from 'redux';
 import { getProducts, productsReducer } from './productsReducer';
-import { userReducer, loginUser, logOutUser, loginSession } from './userReducer';
+import { userReducer, loginUser, logOutUser, loginSession} from './userReducer';
+import {cartReducer, getUsersCart} from './cartReducer';
 
 const rootReducer = combineReducers({
   products: productsReducer,
-  user: userReducer
+  user: userReducer,
+  cart: cartReducer,
 });
 
-export { rootReducer, getProducts, loginUser, logOutUser, loginSession };
+export {
+  rootReducer,
+  getProducts,
+  loginUser,
+  logOutUser,
+  loginSession,
+  getUsersCart,
+};
