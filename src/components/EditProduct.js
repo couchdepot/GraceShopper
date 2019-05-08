4f12120a877774a68019ff778e890bd3c0aefc4a
Alex Bukhman <alexb823@gmail.com>
2019-05-07 21:27:03 -0400

    started on Edit Component

---
+++ src/components/EditProduct.js
@@ -0,0 +1,21 @@
+import React, {useState, useEffect} from 'react';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+
+// name
+// description
+// price
+// quantity
+// imageUrl
+const EditProduct = ({product}) => {
+  return (
+    <Grid container spacing={24}>
+    <form>
+
+    </form>
+    </Grid>
+  )
+}
+
+
